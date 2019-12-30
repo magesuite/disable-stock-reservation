0f6505cb3e7abc51b8d5a1b6bd1dfd1602754315
Paweł Złamaniec <pawel.zlamaniec@creativestyle.pl>
2019-12-30 13:02:44 +0100

    update

--- Test/Integration/Observer/PlaceOrderObserverTest.php
+++ Test/Integration/Observer/PlaceOrderObserverTest.php
@@ -28,11 +28,12 @@ class PlaceOrderObserverTest extends \PHPUnit\Framework\TestCase
 
     private $cartItemFactory;
 
-    private $getStockItemConfiguration;
-
-    private $saveStockItemConfiguration;
-
     private $getSourceItemsBySkuInterface;
+
+    /**
+     * @var \Magento\Framework\App\Config\Storage\WriterInterface
+     */
+    private $writerConfig;
 
     public function setUp()
     {
@@ -49,43 +50,10 @@ class PlaceOrderObserverTest extends \PHPUnit\Framework\TestCase
         $this->stockRepository = $this->objectManager->get(\Magento\Inventory\Model\StockRepository::class);
         $this->storeRepository = $this->objectManager->get(\Magento\Store\Model\StoreRepository::class);
         $this->cartItemFactory = $this->objectManager->get(\Magento\Quote\Api\Data\CartItemInterfaceFactory::class);
-        $this->getStockItemConfiguration = $this->objectManager->get(\Magento\InventoryExportStock\Model\GetStockItemConfiguration::class);
-        $this->saveStockItemConfiguration = $this->objectManager->get(\Magento\InventoryConfiguration\Model\SaveStockItemConfiguration::class);
 
         $this->getSourceItemsBySkuInterface = $this->objectManager->get(\Magento\InventoryApi\Api\GetSourceItemsBySkuInterface::class);
+        $this->writerConfig = $this->objectManager->get(\Magento\Framework\App\Config\Storage\WriterInterface::class);
     }
-
-
-//    /**
-//     * @magentoDataFixture loadWebsiteWithStoreFixture
-//     * @magentoDataFixture loadConfigurableAttributeFixture
-//     * @magentoDataFixture loadProductConfigurableFixture
-//     * @magentoDataFixture loadSourcesFixture
-//     * @magentoDataFixture loadStocksFixture
-//     * @magentoDataFixture loadStockSourceLinksFixture
-//     * @magentoDataFixture loadSourceItemsConfigurableFixture
-//     * @magentoDataFixture loadStockWebsiteSalesChannelsFixture
-//     * @magentoDataFixture loadQuoteFixture
-//     *
-//     * @magentoDbIsolation disabled
-//     */
-//    public function testPlaceOrderWithInStockProduct()
-//    {
-//        $sku = 'configurable';
-//        $qty = 3;
-//
-//        $product = $this->getProductBySku($sku);
-//        $quote = $this->getQuote();
-//        $quote->addProduct($product, $this->getByRequest($product, $qty));
-//        $this->cartRepository->save($quote);
-//        $orderId = $this->cartManagement->placeOrder($quote->getId());
-//
-//        self::assertNotNull($orderId);
-//
-//        $this->deleteOrderById((int)$orderId);
-//    }
-//
-
 
     /**
      * @magentoDbIsolation disabled
@@ -101,54 +69,25 @@ class PlaceOrderObserverTest extends \PHPUnit\Framework\TestCase
      * @magentoDataFixture loadQuoteFixture
      * @magentoDataFixture loadReindexInventoryFixture
      */
-    public function testReduceQuantityAfterOrderFromSingleStock()
+    public function testReduceQuantityAfterOrder()
     {
         $sku = 'SKU-2';
         $qty = 2;
         $stockId = 30;
 
-        $orderId = $this->placeOrder($sku, $qty, $stockId);
-        $this->assertNotNull($orderId);
-
-        $qtyInStock = $this->getSourceItemsBySkuInterface->execute($sku)[9]->getQuantity();
-        $this->assertEquals(3, $qtyInStock);
-
-        $this->deleteOrderById($orderId);
-    }
-
-    /**
-     * @magentoDbIsolation disabled
-     * @magentoAppIsolation enabled
-     *
-     * @magentoDataFixture loadProductsFixture
-     * @magentoDataFixture loadSourcesFixture
-     * @magentoDataFixture loadStocksFixture
-     * @magentoDataFixture loadStockSourceLinksFixture
-     * @magentoDataFixture loadSourceItemsFixture
-     * @magentoDataFixture loadWebsiteWithStoresFixture
-     * @magentoDataFixture loadStockWebsiteSalesChannelsFixture
-     * @magentoDataFixture loadQuoteFixture
-     * @magentoDataFixture loadReindexInventoryFixture
-     */
-    public function testReduceQuantityAfterOrderFromTwoStocks()
-    {
-        $sku = 'SKU-1';
-        $qty = 4;
-        $stockId = 10;
-
-        $orderId = $this->placeOrder($sku, $qty, $stockId);
-        $this->assertNotNull($orderId);
-
-        $qtyInStock = $this->getSourceItemsBySkuInterface->execute($sku)[9]->getQuantity();
-        $this->assertEquals(3, $qtyInStock);
-
-        $this->deleteOrderById($orderId);
-    }
-
-    private function placeOrder($sku, $quoteItemQty, $stockId)
-    {
-        $this->setStockItemConfigIsDecimal($sku, $stockId);
         $cart = $this->getCartByStockId($stockId);
+        $orderId = $this->placeOrder($sku, $qty, $cart);
+        $this->assertNotNull($orderId);
+
+        $qtyInStock = $this->getSourceItemsBySkuInterface->execute($sku)[9]->getQuantity();
+        $this->assertEquals(3, $qtyInStock);
+
+        $this->deleteOrderById($orderId);
+        $this->writerConfig->delete("payone_protect/creditrating/payone_consumerscore_sample_counter", "store", $cart->getStoreId());
+    }
+
+    private function placeOrder($sku, $quoteItemQty, $cart)
+    {
         $product = $this->productRepository->get($sku);
         $cartItem = $this->getCartItem($product, $quoteItemQty, (int)$cart->getId());
         $cart->addItem($cartItem);
@@ -156,21 +95,6 @@ class PlaceOrderObserverTest extends \PHPUnit\Framework\TestCase
 
         $orderId = $this->cartManagement->placeOrder($cart->getId());
         return $orderId;
-    }
-
-    private function setStockItemConfigIsDecimal(string $sku, int $stockId): void
-    {
-        $stockItemConfiguration = $this->getStockItemConfiguration->execute($sku, $stockId);
-        $stockItemConfiguration->setIsQtyDecimal(true);
-        $this->saveStockItemConfiguration->execute($sku, $stockId, $stockItemConfiguration);
-    }
-
-    private function setStockItemManageStockFalse(string $sku, int $stockId): void
-    {
-        $stockItemConfiguration = $this->getStockItemConfiguration->execute($sku, $stockId);
-        $stockItemConfiguration->setManageStock(false);
-        $stockItemConfiguration->setUseConfigManageStock(false);
-        $this->saveStockItemConfiguration->execute($sku, $stockId, $stockItemConfiguration);
     }
 
     private function getCartByStockId($stockId)
@@ -319,89 +243,4 @@ class PlaceOrderObserverTest extends \PHPUnit\Framework\TestCase
     {
         require __DIR__ . "/../../../../../magento/module-inventory-indexer/Test/_files/reindex_inventory.php";
     }
-
-//    public static function loadProductsFixture()
-//    {
-//        require __DIR__ . '/../_files/products.php';
-//    }
-//
-//    public static function loadSourcesFixture()
-//    {
-//        require __DIR__ . '/../_files/sources.php';
-//    }
-//
-//    public static function loadStocksFixture()
-//    {
-//        require __DIR__ . '/../_files/stocks.php';
-//    }
-//
-//    public static function loadStockSourceLinksFixture()
-//    {
-//        require __DIR__ . '/../_files/stock_source_links.php';
-//    }
-//
-//    public static function loadSourceItemsFixture()
-//    {
-//        require __DIR__ . '/../_files/source_items.php';
-//    }
-//
-//    public static function loadWebsitesWithStoresFixture()
-//    {
-//        require __DIR__ . '/../_files/websites_with_stores.php';
-//    }
-//
-//    public static function loadStockWebsiteSalesChannelsFixture()
-//    {
-//        require __DIR__ . '/../_files/stock_website_sales_channels.php';
-//    }
-//
-//    public static function loadQuoteFixture()
-//    {
-//        require __DIR__ . '/../_files/quote.php';
-//    }
-//
-//    public static function loadReindexInventoryFixture()
-//    {
-//        require __DIR__ . '/../_files/reindex_inventory.php';
-//    }
-//
-//    public static function loadProductsFixtureRollback()
-//    {
-//        require __DIR__ . '/../_files/products_rollback.php';
-//    }
-//
-//    public static function loadSourcesFixtureRollback()
-//    {
-//        require __DIR__ . '/../_files/sources_rollback.php';
-//    }
-//
-//    public static function loadStocksFixtureRollback()
-//    {
-//        require __DIR__ . '/../_files/stocks_rollback.php';
-//    }
-//
-//    public static function loadStockSourceLinksFixtureRollback()
-//    {
-//        require __DIR__ . '/../_files/stock_source_links_rollback.php';
-//    }
-//
-//    public static function loadSourceItemsFixtureRollback()
-//    {
-//        require __DIR__ . '/../_files/source_items_rollback.php';
-//    }
-//
-//    public static function loadWebsitesWithStoresFixtureRollback()
-//    {
-//        require __DIR__ . '/../_files/websites_with_stores_rollback.php';
-//    }
-//
-//    public static function loadStockWebsiteSalesChannelsFixtureRollback()
-//    {
-//        require __DIR__ . '/../_files/stock_website_sales_channels_rollback.php';
-//    }
-//
-//    public static function loadQuoteFixtureRollback()
-//    {
-//        require __DIR__ . '/../_files/quote_rollback.php';
-//    }
 }
