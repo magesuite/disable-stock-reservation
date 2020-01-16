f410cb46689f48674ea5065c39d9221066578cbd
Paweł Złamaniec <pawel.zlamaniec@creativestyle.pl>
2020-01-16 10:09:58 +0100

    - Disable MSI plugin that places reservation for orders  - Disable MSI observer that reduces stock qty on shippment  - Extending integration tests

--- Test/Integration/Plugin/InventorySalesApi/Api/PreventAddingDefaultCompensationTest.php
+++ Test/Integration/Plugin/InventorySalesApi/Api/PreventAddingDefaultCompensationTest.php
@@ -53,6 +53,47 @@ class PreventAddingDefaultCompensationTest extends \MageSuite\DisableStockReserv
 
         $shipment->register();
         $shipment->getOrder()->setIsInProcess(true);
+        $shipment->save();
+
+        $qty = $this->getReservationQuantity->execute($sku, $stockId);
+        $this->assertEquals(0, $qty);
+
+        $this->deleteOrderById($orderId);
+    }
+
+    /**
+     * @magentoDbIsolation  disabled
+     * @magentoAppIsolation enabled
+     *
+     * @magentoDataFixture loadProductsFixture
+     * @magentoDataFixture loadSourcesFixture
+     * @magentoDataFixture loadStocksFixture
+     * @magentoDataFixture loadStockSourceLinksFixture
+     * @magentoDataFixture loadSourceItemsFixture
+     * @magentoDataFixture loadWebsiteWithStoresFixture
+     * @magentoDataFixture loadStockWebsiteSalesChannelsFixture
+     * @magentoDataFixture loadQuoteFixture
+     * @magentoDataFixture loadReindexInventoryFixture
+     */
+    public function testReservationsAfterShipmentOfAllQtyFromStock()
+    {
+        $sku = 'SKU-2';
+        $qty = 5;
+        $stockId = 30;
+
+        $cart = $this->getCartByStockId($stockId);
+        $orderId = $this->placeOrder($sku, $qty, $cart);
+        $order = $this->orderRepository->get($orderId);
+
+        $shipment = $this->convertOrder->toShipment($order);
+        foreach ($order->getItems() as $item) {
+            $shipmentItem = $this->convertOrder->itemToShipmentItem($item)->setQty($qty);
+            $shipment->addItem($shipmentItem);
+        }
+
+        $shipment->register();
+        $shipment->getOrder()->setIsInProcess(true);
+        $shipment->save();
 
         $qty = $this->getReservationQuantity->execute($sku, $stockId);
         $this->assertEquals(0, $qty);
