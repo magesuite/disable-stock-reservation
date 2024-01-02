ccbfa3e0949056b856bebd56fef4561845b059ab
Iwona Jóźwiak <iwona.jozwiak@creativestyle.pl>
2024-01-02 13:48:32 +0000

    fix: [MGP-1129] Exclude deleted products from order cancellation and stock replenishment process

--- Test/Integration/Observer/ReturnQtyToStockTest.php
+++ Test/Integration/Observer/ReturnQtyToStockTest.php
@@ -37,4 +37,56 @@ class ReturnQtyToStockTest extends \MageSuite\DisableStockReservation\Test\Integ
         $qtyInStock = current($this->getSourceItemsBySkuInterface->execute($sku))->getQuantity();
         $this->assertEquals(5, $qtyInStock);
     }
+
+    /**
+     * @magentoAppArea adminhtml
+     * @magentoDbIsolation  disabled
+     * @magentoAppIsolation enabled
+     *
+     * @magentoDataFixture loadWebsiteWithStoresFixture
+     * @magentoDataFixture loadProductsFixture
+     * @magentoDataFixture loadSourcesFixture
+     * @magentoDataFixture loadStocksFixture
+     * @magentoDataFixture loadStockSourceLinksFixture
+     * @magentoDataFixture loadSourceItemsFixture
+     * @magentoDataFixture loadStockWebsiteSalesChannelsFixture
+     * @magentoDataFixture loadQuoteFixture
+     * @magentoDataFixture loadReindexInventoryFixture
+     */
+    public function testReturnQtyToStockAfterOrderCancelWhenProductDoesntExist()
+    {
+        $sku = 'SKU-2';
+        $qty = 2;
+        $stockId = 30;
+
+        $cart = $this->getCartByStockId($stockId);
+        $orderId = $this->placeOrder($sku, $qty, $cart);
+        $this->assertNotNull($orderId);
+
+        $order = $this->orderRepository->get($orderId);
+        $getItemsToReturn = $this->objectManager->create(\MageSuite\DisableStockReservation\Service\GetItemsToReturn::class);
+
+        $orderItemsSkus = $getItemsToReturn->execute($order);
+        $this->assertNotEmpty($orderItemsSkus);
+        $this->assertEquals(['SKU-2' => 2.0], $orderItemsSkus);
+
+        $this->productRepository->deleteById($sku);
+        $orderItemsSkus = $getItemsToReturn->execute($order);
+        $this->assertEmpty($orderItemsSkus);
+
+        try {
+            $order->cancel();
+        } catch (\Exception $e) {
+            $this->fail(sprintf('Order cancellation failed: An exception occurred: %s', $e->getMessage()));
+        }
+    }
+
+    protected function reindexStock()
+    {
+        /** @var \Magento\Indexer\Model\IndexerFactory $indexerFactory */
+        $indexerFactory = $this->objectManager->create(\Magento\Indexer\Model\IndexerFactory::class);
+        $indexer = $indexerFactory->create();
+        $indexer->load('inventory');
+        $indexer->reindexAll();
+    }
 }
